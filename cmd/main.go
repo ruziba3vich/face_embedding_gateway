7b9c9e7b2181ffb50a4c1221b45c2af84e373e78
ruziba3vich <rruzibayevich@gmail.com>
2025-06-23 13:28:48 +0500

    feat: implement routers

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/ruziba3vich/face_embedding_gateway/genprotos/face_recognition_service"
 	handler "github.com/ruziba3vich/face_embedding_gateway/internal/http"
 	"github.com/ruziba3vich/face_embedding_gateway/internal/service"
+	"github.com/ruziba3vich/face_embedding_gateway/pkg/helper"
 	lgg "github.com/ruziba3vich/prodonik_lgger"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -28,11 +29,22 @@ func main() {
 
 	faceRecognitionHandler := handler.NewFaceRecognitionHandler(client, logger)
 
-	userService := service.NewService(client, )
-	userHandler := handler.NewUserHandler()
+	db, err := helper.NewDB("")
+
+	milvusClient, err := helper.CreateMilvusClientAndCollection("face_embeddings", "", "512")
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	userService := service.NewService(db, milvusClient)
+	userHandler := handler.NewUserHandler(userService, client, logger)
 
 	router := gin.Default()
 	router.POST("/embedd", faceRecognitionHandler.HandleImageEmbedding)
+	router.POST("/ceate-user", userHandler.CreateUser)
+	router.GET("/get-user", userHandler.GetUserByID)
+	router.DELETE("/delete-user", userHandler.DeleteUserByID)
+	router.POST("/create-pic", userHandler.HandleImageEmbedding)
 
 	if err := router.Run(":7777"); err != nil {
 		logger.Fatal(err)
